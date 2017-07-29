eea16763bccc16aca7309215983b728f16e2df49
Ryan Perry <Rperry2174@gmail.com>
2017-07-29 14:40:39 -0700

    added shell for satori component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import Login from './components/Login.jsx'
 import Register from './components/Register/Register.jsx'
 import Profile from './components/Profile.jsx'
-import Event from './components/Event.jsx'
-import Home from './components/Home/home.jsx'
-
+import Event from './components/Event/Event.jsx'
+import Home from './components/home.jsx'
+import Satori from './components/Satori/Satori.jsx'
 import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { browserHistory } from 'react-router'
 
@@ -26,6 +26,7 @@ class App extends React.Component {
               <Route exact path="/register" component={Register}></Route>
               <Route exact path="/profile" component={Profile}></Route>
               <Route exact path="/event" component={Event}></Route>
+              <Route exact path="/satori" component={Satori}></Route>
             </Switch>
         </Router>
       </div>
