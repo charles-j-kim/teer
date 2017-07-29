4332acc3259db83b73578c4f032fd2e143666358
Ryan Perry <Rperry2174@gmail.com>
2017-07-29 15:42:28 -0700

    fixed minor bug

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,7 @@ import Login from './components/Login.jsx'
 import Register from './components/Register/Register.jsx'
 import Profile from './components/Profile.jsx'
 import Event from './components/Event/Event.jsx'
-import Home from './components/home.jsx'
+import Home from './components/Home/home.jsx'
 import Satori from './components/Satori/Satori.jsx'
 import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { browserHistory } from 'react-router'
