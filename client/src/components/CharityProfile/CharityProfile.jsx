5a7b7d96bb46e608129cb81ea23cf4c3dfa6dfac
Charles Kim <cjkim0119@gmail.com>
2017-07-29 16:22:04 -0700

    Add reviews portion of CharityProfile view

---
+++ client/src/components/CharityProfile/CharityProfile.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { withRouter} from 'react-router';
+import About from './About.jsx';
+import Reviews from './Reviews.jsx';
+
+class CharityProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <img src="https://targetcareers.co.uk/sites/targetcareers.co.uk/files/public/field/image/Types-of-jobs-and-employers-in-charity-work.jpg" />
+        <img src="http://www.retail-merchandiser.com/images/AMERICANRED.jpg" />
+        <div>
+          <span>American Red Cross</span>
+          <span>San Francisco, CA, United States</span>
+        </div>
+        <div>
+          <span>About</span>
+          <span>Reviews</span>
+          <span>Upcoming Events</span>
+          <span>Past Events</span>
+          <span>Location</span>
+        </div>
+        <div>
+          <About />
+          <Reviews />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(CharityProfile);
