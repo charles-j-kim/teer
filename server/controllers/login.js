884118a0b9312c826262f8778c3054fc5baf2645
kale007 <rajas.p.kale@gmail.com>
2017-07-29 15:34:30 -0700

    starts setting up login autho verification

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,3 +1,17 @@
+var models = require('../../db/models');
+
 module.exports.login = function (req, res, next) {
-  res.status(200).send('WE GONN\' LOGIN YO!');
+  // res.status(200).send('WE GONN\' LOGIN YO!');
+  // forge Users table
+  console.log(req.body)
+  models.User.forge()
+  .where( {email: req.query.email} )
+  .fetchAll()
+  .then(function(person) {
+    console.log(person);
+  })
+  // look up email
+  // check if username matches
+    // if yes, send status 200
+    // else send Forbidden status
 }
