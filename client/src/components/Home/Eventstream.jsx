180cd2fe2b5a78a310e5b920429ff6ac8026c97a
kale007 <rajas.p.kale@gmail.com>
2017-07-30 09:06:10 -0700

    fix fix fix

--- client/src/components/Home/Eventstream.jsx
+++ client/src/components/Home/Eventstream.jsx
@@ -13,18 +13,14 @@ class Eventstream extends React.Component {
 
     return (
     	<div className="eventstream">
-    		{events.map((eventItem,i)=>
+    		{events.map((eventItem, i)=>
     			<Event
-<<<<<<< HEAD
-                    key={i}
-    				eventImage={eventItem.img_url}
-=======
-    				eventImage={eventItem.event_img_url}
->>>>>>> fixes date-time rendering
+						eventImage={eventItem.event_img_url}
     				eventTitle={eventItem.name}
     				date={String(eventItem.start_date_hr).slice(0,10)}
     				organisation={eventItem.org_name}
     				eventId={eventItem.id}
+						key = {i}
     			/>
     		)}
     	</div>
