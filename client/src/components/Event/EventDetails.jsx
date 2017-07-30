5ddcc0e9557fe2e27867a4f642f5c1cd93487f19
Charles Kim <cjkim0119@gmail.com>
2017-07-30 09:23:20 -0700

    Refactor date in event label

--- client/src/components/Event/EventDetails.jsx
+++ client/src/components/Event/EventDetails.jsx
@@ -7,15 +7,36 @@ class EventDetails extends React.Component {
     this.state = {}
   }
 
-  componentWillMount() {
-
-  }
-
   render () {
+    const days = {
+      0: 'Sunday',
+      1: 'Monday',
+      2: 'Tuesday',
+      3: 'Wednesday',
+      4: 'Thursday',
+      5: 'Friday',
+      6: 'Saturday',
+    };
+    const months = {
+      0: 'January',
+      1: 'February',
+      2: 'March',
+      3: 'April',
+      4: 'May',
+      5: 'June',
+      6: 'July',
+      7: 'August',
+      8: 'September',
+      9: 'October',
+      10: 'November',
+      11: 'December',
+    };
+    let eventDate = new Date(this.props.eventDate);
+
     return (
       <div>
         <h1> {this.props.eventTitle} </h1>
-        <h3> {this.props.eventDate} </h3>
+        <h3> {days[eventDate.getDay()]}, {months[eventDate.getMonth()]} {eventDate.getDate()}, {eventDate.getFullYear()} </h3>
         <p> {this.props.eventDescription}</p>
 
         {/* <h3> What to bring </h3>
