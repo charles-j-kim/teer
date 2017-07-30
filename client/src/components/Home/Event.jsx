98dc397bb081f78e3f4a9f5765307cefb31748a8
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-07-30 08:57:46 -0700

    Added so that a lot of stuff are clickable.

--- client/src/components/Home/Event.jsx
+++ client/src/components/Home/Event.jsx
@@ -7,6 +7,7 @@ class Event extends React.Component {
 		super(props);
 		this.state = {}
     this.handleClick = this.handleClick.bind(this);
+    this.clickOrg = this.clickOrg.bind(this);
 	}
 
   handleClick(e) {
@@ -19,21 +20,31 @@ class Event extends React.Component {
     });
   }
 
+  clickOrg() {
+    var eventID = this.props.charityId;
+    this.props.history.push({
+      pathname: '/charityprofile',
+      state: {
+        charityID: eventID
+      }
+    });
+  }
+
 	render () {
     return (
-    	<div className="event" onClick={this.handleClick}>
+    	<div className="event">
           <div className="event-image-container">
-          <img className="event-image" src={this.props.eventImage}></img>
+          <img className="event-image" src={this.props.eventImage} onClick={this.handleClick}></img>
           </div>
         <div className="event-details">
-          <div id="event-title">
+          <div id="event-title" onClick={this.handleClick}>
       		  {this.props.eventTitle}
           </div>
-          <div id="event-date">
+          <div id="event-date" onClick={this.handleClick}>
             {this.props.date}
           </div>
           <hr/>
-          <div id="organizer">
+          <div id="organizer" onClick={this.clickOrg}>
             {this.props.organisation}
         </div>
       </div>
